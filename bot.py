ff3fa63c2071684b853937e99a26a495d88c4302
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2023-12-01 12:55:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,29 +5,40 @@ import os
 import datetime
 
 def get_text():
-    # Получаем сегодняшнюю дату
-    today = datetime.date.today()
+    # Переменные для времени отправки (17:00)
+    send_hour = 17
+    send_minute = 0
     
-    # Добавляем один день к текущей дате, чтобы получить следующий день
-    next_day = today + datetime.timedelta(days=1)
-    next_day_day = next_day.day
-    next_day_month = next_day.month
+    # Получаем текущие часы и минуты
+    current_hour = datetime.datetime.now().hour
+    current_minute = datetime.datetime.now().minute
+
+    # Проверка текущего времени для определения даты для отправки
+    if current_hour > send_hour or (current_hour == send_hour and current_minute >= send_minute):
+        # Если текущее время больше или равно 17:00, отправляем текст за следующий день
+        today = datetime.date.today() + datetime.timedelta(days=1)
+    else:
+        # Иначе отправляем текст за сегодняшний день
+        today = datetime.date.today()
+
+    current_day = today.day
+    current_month = today.month
 
     # Путь к папке "book"
     folder_path = 'book'
 
-    # Путь к папке следующего месяца
-    month_folder_path = os.path.join(folder_path, str(next_day_month))
+    # Путь к папке текущего месяца
+    month_folder_path = os.path.join(folder_path, str(current_month))
 
-    # Формируем имя файла следующего дня (например, "1.txt", "2.txt", ..., "31.txt")
-    next_day_file = f"{next_day_day}.txt"
+    # Формируем имя файла сегодняшнего дня (например, "1.txt", "2.txt", ..., "31.txt")
+    today_file = f"{current_day}.txt"
 
-    # Проверяем существование папки следующего месяца
+    # Проверяем существование папки месяца
     if os.path.exists(month_folder_path):
-        # Путь к файлу следующего дня в следующем месяце
-        file_path = os.path.join(month_folder_path, next_day_file)
+        # Путь к файлу сегодняшнего дня в текущем месяце
+        file_path = os.path.join(month_folder_path, today_file)
 
-        # Проверяем существование файла следующего дня
+        # Проверяем существование файла сегодняшнего дня
         if os.path.exists(file_path):
             # Открываем файл и считываем его содержимое
             with open(file_path, 'r', encoding='utf-8') as file:
@@ -51,9 +62,9 @@ def get_text():
                 # Возвращаем обработанный текст
                 return formatted_content
         else:
-            return f"Файл для {next_day_day} числа не найден в папке месяца {next_day_month}."
+            return f"Файл для {current_day} числа не найден в папке месяца {current_month}."
     else:
-        return f"Папка для месяца {next_day_month} не найдена в папке 'book'."
+        return f"Папка для месяца {current_month} не найдена в папке 'book'."
 
 
 async def main():
