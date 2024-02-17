e0ee1c69fb4dd3902472bba82befa39ffc7d02a6
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2024-02-17 15:12:53 +0300

    Update bot.py
    + постинг в АА

--- bot.py
+++ bot.py
@@ -5,75 +5,128 @@ import os
 import datetime
 
 def get_text():
-    # Переменные для времени отправки (17:00)
+    # часы запуска скрипта на гитхабе
     send_hour = 17
     send_minute = 0
     
-    # Получаем текущие часы и минуты
     current_hour = datetime.datetime.now().hour
     current_minute = datetime.datetime.now().minute
 
-    # Проверка текущего времени для определения даты для отправки
     if current_hour > send_hour or (current_hour == send_hour and current_minute >= send_minute):
-        # Если текущее время больше или равно 17:00, отправляем текст за следующий день
         today = datetime.date.today() + datetime.timedelta(days=1)
     else:
-        # Иначе отправляем текст за сегодняшний день
         today = datetime.date.today()
 
     current_day = today.day
     current_month = today.month
 
-    # Путь к папке "book"
     folder_path = 'book'
 
-    # Путь к папке текущего месяца
     month_folder_path = os.path.join(folder_path, str(current_month))
 
-    # Формируем имя файла сегодняшнего дня (например, "1.txt", "2.txt", ..., "31.txt")
     today_file = f"{current_day}.txt"
 
-    # Проверяем существование папки месяца
     if os.path.exists(month_folder_path):
-        # Путь к файлу сегодняшнего дня в текущем месяце
         file_path = os.path.join(month_folder_path, today_file)
 
-        # Проверяем существование файла сегодняшнего дня
         if os.path.exists(file_path):
-            # Открываем файл и считываем его содержимое
             with open(file_path, 'r', encoding='utf-8') as file:
                 content = file.read()
 
-                # Разделяем содержимое на абзацы
                 paragraphs = content.split('\n\n')
 
-                # Находим самый большой абзац
                 largest_paragraph = max(paragraphs, key=len)
 
-                # Разделяем содержимое самого большого абзаца на строки
                 lines = largest_paragraph.splitlines()
 
-                # Добавляем по две пустые строки до и после самого большого абзаца
                 formatted_paragraph = '\n' + '\n\n'.join(lines) + '\n'
 
-                # Заменяем самый большой абзац в тексте на разделенный по строкам
                 formatted_content = content.replace(largest_paragraph, formatted_paragraph)
 
-                # Возвращаем обработанный текст
                 return formatted_content
         else:
             return f"Файл для {current_day} числа не найден в папке месяца {current_month}."
     else:
         return f"Папка для месяца {current_month} не найдена в папке 'book'."
 
-
 async def main():
+    # постинг в канал "Светский ежедневник"
     bot_token = '6541742098:AAE-hirxm_Dtl-9kQMAcRjlWGpy_JwQ2rYQ'
     chat_id = '@SecularNA'
     message_to_send = get_text()
 
     bot = Bot(token=bot_token)
     await bot.send_message(chat_id=chat_id, text=message_to_send)
+    
+    # постинг в канал "Реалисты"
+    replacements = [
+        {'keyword': 'АН', 'replaceword': 'АА'},
+
+        {'keyword': 'Анонимные Наркоманы', 'replaceword': 'Анонимные Алкоголики'},
+        {'keyword': 'Анонимные наркоманы', 'replaceword': 'Анонимные алкоголики'},
+        {'keyword': 'анонимные наркоманы', 'replaceword': 'анонимные алкоголики'},
+        {'keyword': 'анонимные Наркоманы', 'replaceword': 'анонимные Алкоголики'},
+
+        {'keyword': 'Анонимных Наркоманов', 'replaceword': 'Анонимных Алкоголиков'},
+        {'keyword': 'Анонимных наркоманов', 'replaceword': 'Анонимных алкоголиков'},
+        {'keyword': 'анонимных наркоманов', 'replaceword': 'анонимных алкоголиков'},
+        {'keyword': 'анонимных Наркоманов', 'replaceword': 'анонимных Алкоголиков'},
+
+        {'keyword': 'Анонимным Наркоманам', 'replaceword': 'Анонимным Алкоголикам'},
+        {'keyword': 'Анонимным наркоманам', 'replaceword': 'Анонимным алкоголикам'},
+        {'keyword': 'анонимным наркоманам', 'replaceword': 'анонимным алкоголикам'},
+        {'keyword': 'анонимным Наркоманам', 'replaceword': 'анонимным Алкоголикам'},
+
+        {'keyword': 'Анонимными Наркоманами', 'replaceword': 'Анонимными Алкоголиками'},
+        {'keyword': 'Анонимными наркоманами', 'replaceword': 'Анонимными алкоголиками'},
+        {'keyword': 'анонимными наркоманами', 'replaceword': 'анонимными алкоголиками'},
+        {'keyword': 'анонимными Наркоманами', 'replaceword': 'анонимными Алкоголиками'},
+
+        {'keyword': 'Наркоманов',   'replaceword': 'Алкоголиков'},
+        {'keyword': 'Наркоманы',    'replaceword': 'Алкоголики'},
+        {'keyword': 'Наркоман',     'replaceword': 'Алкоголик'},
+
+        {'keyword': 'наркоманов',   'replaceword': 'алкоголиков'},
+        {'keyword': 'наркоманы',    'replaceword': 'алкоголики'},
+        {'keyword': 'наркоман',     'replaceword': 'алкоголик'},
+
+        {'keyword': 'Наркотиков',       'replaceword': 'Алкоголя'},
+        {'keyword': 'Наркотики',        'replaceword': 'Алкоголь'},
+        {'keyword': 'Наркотиками',      'replaceword': 'Алкоголем'},
+        {'keyword': 'Наркотик',         'replaceword': 'Алкоголь'},
+
+        {'keyword': 'наркотиков',       'replaceword': 'алкоголя'},
+        {'keyword': 'наркотики',        'replaceword': 'алкоголь'},
+        {'keyword': 'наркотиками',      'replaceword': 'алкоголем'},
+        {'keyword': 'наркотик',         'replaceword': 'алкоголь'},
+
+        {'keyword': 'Наркозависимость',     'replaceword': 'Алкозависимость'},
+        {'keyword': 'Наркозависимости',     'replaceword': 'Алкозависимости'},
+        {'keyword': 'Наркозависимый',       'replaceword': 'Алкозависимый'},
+        {'keyword': 'Наркозависимых',       'replaceword': 'Алкозависимых'},
+        {'keyword': 'Наркозависимому',      'replaceword': 'Алкозависимому'},
+        {'keyword': 'Наркозависимыми',      'replaceword': 'Алкозависимыми'},
+
+        {'keyword': 'наркозависимость',     'replaceword': 'алкозависимость'},
+        {'keyword': 'наркозависимости',     'replaceword': 'алкозависимости'},
+        {'keyword': 'наркозависимый',       'replaceword': 'алкозависимый'},
+        {'keyword': 'наркозависимых',       'replaceword': 'алкозависимых'},
+        {'keyword': 'наркозависимому',      'replaceword': 'алкозависимому'},
+        {'keyword': 'наркозависимыми',      'replaceword': 'алкозависимыми'},
+
+        {'keyword': '', 'replaceword': ''}
+    ]
+    message_to_send_2 = get_text()
+
+    for replacement in replacements:
+        keyword = replacement['keyword']
+        replaceword = replacement['replaceword']
+        message_to_send_2 = message_to_send_2.replace(keyword, replaceword)
+
+    bot_token_2 = '6541742098:AAE-hirxm_Dtl-9kQMAcRjlWGpy_JwQ2rYQ'
+    chat_id_2 = '-1002137516831'
+
+    bot = Bot(token=bot_token_2)
+    await bot.send_message(chat_id=chat_id_2, text=message_to_send_2)
 
 asyncio.run(main())
-
