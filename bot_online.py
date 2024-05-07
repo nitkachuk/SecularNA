6afd26af6c2b4dcb3281d21ac37ec36f74c0d2ee
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2024-05-07 23:27:34 +0300

    Add files via upload

--- bot_online.py
+++ bot_online.py
@@ -1,12 +1,34 @@
 import os
 import signal
 import asyncio
-from telegram import Update
-from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+from telegram import Bot, Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from g4f.client import Client
 
 # Получаем токен из переменной среды TELEGRAM_TOKEN
 telegram_token = os.getenv('TELEGRAM_TOKEN')
+# Указываем ID канала, куда будем постить сообщения
+channel_id = '@my_bot_test_666'
+
+# Функция для отправки сообщения в канал
+def send_message(message):
+    bot = Bot(token=telegram_token)
+    bot.send_message(chat_id=channel_id, text=message)
+
+# Функция для отправки сообщения при остановке бота
+def stop_bot():
+    send_message("bot stop")
+
+# Обработчик события SIGTERM для отправки сообщения перед остановкой
+def handle_sigterm(signum, frame):
+    stop_bot()
+    raise SystemExit
+
+# Регистрируем обработчик события SIGTERM
+signal.signal(signal.SIGTERM, handle_sigterm)
+
+# Отправляем сообщение о запуске бота
+send_message("bot start")
 
 # Инициализация клиента для GPT-3.5
 client = Client()
@@ -47,3 +69,6 @@ async def main():
 
 # Запускаем асинхронный цикл
 asyncio.run(main())
+
+# Перед завершением работы отправляем сообщение в канал
+stop_bot()
