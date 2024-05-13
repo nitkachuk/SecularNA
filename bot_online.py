fbdf30d8ba413118b851a0f442a53b8ca4bcbd49
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2024-05-13 10:17:20 +0300

    Update bot_online.py

--- bot_online.py
+++ bot_online.py
@@ -3,26 +3,30 @@ import asyncio
 import telebot
 from g4f.client import Client
 
-# установка временной переменной среды для yml-файла с проверкой
-os.environ['BOT_ONLINE'] = '1'
-
 telegram_token = os.getenv('TELEGRAM_TOKEN')
-bot = telebot.TeleBot( telegram_token )
+bot = telebot.TeleBot(telegram_token)
 client = Client()
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     sent_message = bot.reply_to(message, 'Секундочку...')  # ответ 1
 
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[ 
-            {"role": "system", "content": 'ответь на запрос по-русски'},
-            {"role": "user", "content": message.text}
-        ],
-    )
+    try:
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": 'ответь на запрос по-русски'},
+                {"role": "user", "content": message.text}
+            ],
+        )
 
-    bot.delete_message(message.chat.id, sent_message.message_id)  # удаление ответа 1
-    bot.reply_to(message, completion.choices[0].message.content)  # ответ 2
+        if completion.choices:
+            bot.delete_message(message.chat.id, sent_message.message_id)  # удаление ответа 1
+            bot.reply_to(message, completion.choices[0].message.content)  # ответ 2
+        else:
+            bot.reply_to(message, "Сбой обращения к нейронной сети: получен пустой ответ")
+    except Exception as e:
+        bot.delete_message(message.chat.id, sent_message.message_id)  # удаление ответа 1
+        bot.reply_to(message, "Сбой обращения к нейронной сети: " + str(e))  # ответ 3
 
 bot.polling()
