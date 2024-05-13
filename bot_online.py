bd81525a798fd81acb170772d8808c97166c9bfd
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2024-05-14 00:08:02 +0300

    Update bot_online.py

--- bot_online.py
+++ bot_online.py
@@ -16,38 +16,46 @@ def has_glyphs(text):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
+    attempt_count = 0  # счетчик попыток отправки
     while True:
         try:
-            sent_message = bot.reply_to(message, 'Секундочку...')  # ответ 1
+            attempt_count += 1  # увеличение счетчика попыток
+            if attempt_count > 1:
+                sent_message = bot.reply_to(message, f'Секундочку... #{attempt_count}')  # ответ 1
+            else:
+                sent_message = bot.reply_to(message, 'Секундочку...')  # ответ 1
 
             completion = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=[
-                    {"role": "system", "content": 'ответь на запрос без иероглифов'},
+                    #{"role": "system", "content": ''},
                     {"role": "user", "content": message.text}
                 ],
             )
 
             if has_glyphs(completion.choices[0].message.content):
-                continue  # Пропускаем отправку сообщения и повторяем цикл
+                bot.delete_message(message.chat.id, sent_message.message_id)  # Удаление сообщения "Секундочку..."
+                continue
 
-            bot.delete_message( message.chat.id, sent_message.message_id )  # удаление ответа 1
-            bot.reply_to( message, completion.choices[0].message.content )  # ответ 2
+            bot.delete_message(message.chat.id, sent_message.message_id)  # Удаление сообщения "Секундочку..."
+            bot.reply_to(message, completion.choices[0].message.content)  # ответ 2
 
-            print( completion.choices[0].message )
-
-            break  # выход из цикла while после успешного выполнения
+            break
 
         except telebot.apihelper.ApiTelegramException as e:
             # Обработка исключения, чтобы скрипт не завершался при ошибке API Telegram
             err = "Произошла ошибка API Telegram"
             print(err, e)
-            continue  # переход на следующую итерацию цикла while
+            bot.delete_message(message.chat.id, sent_message.message_id)  # Удаление сообщения "Секундочку..."
+            continue
 
         except Exception as e:
             # Другие исключения
             err = "Произошла неизвестная ошибка"
             print(err, e)
-            continue  # переход на следующую итерацию цикла while
+            bot.delete_message(message.chat.id, sent_message.message_id)  # Удаление сообщения "Секундочку..."
+            continue 
+
+    attempt_count = 0  # сброс счетчика попыток после успешной отправки
 
 bot.polling()
