f44065d55cf47946ca12967ecfbc4ecaca67b838
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2024-05-13 18:37:45 +0300

    Update bot_online.py

--- bot_online.py
+++ bot_online.py
@@ -9,24 +9,46 @@ client = Client()
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    sent_message = bot.reply_to(message, 'Секундочку...')  # ответ 1
-
     try:
+        sent_message = bot.reply_to(message, 'Секундочку...')  # ответ 1
+
         completion = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": 'ответь на запрос по-русски'},
+                {"role": "system", "content": 'ответь на запрос без иероглифов'},
                 {"role": "user", "content": message.text}
             ],
         )
 
-        if completion.choices:
-            bot.delete_message(message.chat.id, sent_message.message_id)  # удаление ответа 1
-            bot.reply_to(message, completion.choices[0].message.content)  # ответ 2
-        else:
-            bot.reply_to(message, "Сбой обращения к нейронной сети: получен пустой ответ")
+        bot.delete_message( message.chat.id, sent_message.message_id )  # удаление ответа 1
+        bot.reply_to( message, completion.choices[0].message.content )  # ответ 2
+
+        print( completion.choices[0].message )
+
+    except telebot.apihelper.ApiTelegramException as e:
+        # Обработка исключения, чтобы скрипт не завершался при ошибке API Telegram
+        err = "Произошла ошибка API Telegram"
+        print(err, e)
+
+        # try:
+        #     bot.reply_to(message, err)
+        # except:
+        #     pass
+
+        # print( "ответ нейронки: ", sent_message.message_id )
+
+        echo_all(message)
+
     except Exception as e:
-        bot.delete_message(message.chat.id, sent_message.message_id)  # удаление ответа 1
-        bot.reply_to(message, "Сбой обращения к нейронной сети: " + str(e))  # ответ 3
+        # Другие исключения
+        err = "Произошла неизвестная ошибка"
+        print(err, e)
+        
+        # try:
+        #     bot.reply_to(message, err)
+        # except:
+        #     pass
+
+        echo_all(message)
 
 bot.polling()
