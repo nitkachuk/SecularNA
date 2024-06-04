0b6f2910c2b70d3c25ac096f756e678dd7219357
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2024-06-04 10:19:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -185,10 +185,6 @@ async def main():
             print("role_user in message. try again...")
             continue
 
-        if len( message_to_send_3.split("\n\n") ) > 3:
-            print("once again")
-            continue
-
         await bot.send_message( chat_id=chat_id_3, text=message_to_send_3 )
         break
 
