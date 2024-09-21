6fc41fac752418bc6fa403d5b19b40b5bff469f5
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2024-09-21 14:21:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -364,7 +364,7 @@ async def main():
             ],
         )
 
-        ai_response = "*__Принципы программы__* 🙏 \n\n" +completion.choices[0].message.content
+        ai_response = "*__Принципы программы__* 🌱 \n\n" +completion.choices[0].message.content
 
         if has_glyphs(ai_response):
             print("has glyphs. try again... ⚙️ \n")
