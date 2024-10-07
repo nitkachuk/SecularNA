c69f8fabda585816a0e35d41bb6db828eaebc671
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2024-10-07 18:27:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import asyncio
 from telegram import Bot
 from g4f.client import Client
 
+import os
+
 from replacements import replacements, doReplacements # type: ignore
 from state import has_glyphs, escape_markdown_v2, escape_system_text, readTheBook  # type: ignore
 
