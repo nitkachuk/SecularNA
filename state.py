c80a6758cba5d016b9778673ed3d74db621719b2
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2024-10-07 23:26:58 +0300

    Update state.py

--- state.py
+++ state.py
@@ -110,9 +110,9 @@ async def telegramPost( chat_id, message_to_send, title ):
     
     try:
         await bot.send_message(chat_id=chat_id, text=message_to_send, parse_mode='MarkdownV2')
-        print( f"\n\n{title} ✅", flush=True )
+        print( f"{title} ✅", flush=True )
     except Exception as e:
-        print( f"\n\n{title} ❌", flush=True )
+        print( f"{title} ❌", flush=True )
         print( "Ошибка тг:", e, " ⚙️ \n", flush=True )
         attempts += 1
 
