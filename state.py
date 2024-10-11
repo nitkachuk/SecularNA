c759dab0be022a9841cf69500c947f6384eed40a
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2024-10-11 21:23:20 +0300

    Update state.py

--- state.py
+++ state.py
@@ -117,14 +117,14 @@ def readTheBook():
         return f"Папка для месяца {current_month} не найдена в папке 'book'."
 
 
-def createMessage( text, title, emoji, title2 = '' ):
-    title = escape_markdown_v2( title )
+def createMessage( text, title, title2 = '' ):
+    title = escape_markdown_v2( title[0] ), title[1]
     
     if title2:
         title2 = f"_{title2}_\n\n"
     title2 = escape_markdown_v2( title2 )
         
-    return f"\n\n*__{title}__* {emoji}\n\n{title2}{text}"
+    return f"\n\n*__{title[0]}__* {title[1]}\n\n{title2}{text}"
 
 
 async def telegramPost( chat_id, message_to_send, title ):
