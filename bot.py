e68c25ae00c6b50b4dd0d36cd790bf845eb585a3
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2024-10-19 06:48:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from g4f.client import Client
 from replacements import replacements, doReplacements  # type: ignore
 from state import attempts, has_glyphs, escape_markdown_v2, escape_system_text, \
     readTheBook, telegramPost, aiRequest, channelBook, channelBill, \
-    createMessage   # type: ignore
+    createMessage, checkNAholiday   # type: ignore
 from roulette import getRandomTheme   # type: ignore
 
 
