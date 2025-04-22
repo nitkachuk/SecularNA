918c31f6bd2da55ffddb14542e4cc8654943d18e
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2025-04-23 00:01:16 +0300

    Update bot_online.py

--- bot_online.py
+++ bot_online.py
@@ -16,7 +16,8 @@ def has_glyphs(text):
 #@bot.message_handler(func=lambda message: True)
 #@bot.message_handler(func=lambda message: message.from_user.username == 'kristina_superstar')
 
-@bot.message_handler(func=lambda message: message.from_user.username in ['kristina_superstar', 'gothicspring'])
+@bot.message_handler(func=lambda message: message.from_user.username _
+            in ['kristina_superstar', 'gothicspring', 'Kungfuoko'])
 
 def echo_all(message):
     attempt_count = 0  # счетчик попыток отправки
