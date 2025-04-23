ab0183c780ef219326bde88fa17bab5ff479a0f3
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2025-04-23 13:13:45 +0300

    Update bot_online.py

--- bot_online.py
+++ bot_online.py
@@ -47,7 +47,7 @@ def echo_all(message):
                 
                 try:
                     response = future.result(timeout=10)  # Таймаут 10 секунд
-                    if time.time() - start_time > 5:
+                    if time.time() - start_time > 1:
                         bot.delete_message(message.chat.id, sent_message.message_id)  # Удаление сообщения "Секундочку..."
                         bot.reply_to(message, "Ошибка нейросети — нет ответа от сервера ❌")  # Краткий ответ о долгом ожидании
                         break  # Прерываем цикл
