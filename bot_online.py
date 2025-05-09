988111f6fa6c0c123e529ddfa79ee2645730b1e7
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2025-05-10 02:09:20 +0300

    Update bot_online.py

--- bot_online.py
+++ bot_online.py
@@ -47,20 +47,20 @@ def g4f_with_timeout(txt, timeout=10):
         raise result
     return result
 
-# def delete_last_message():
-#     try:
-#         bot.delete_message(globalMessageObject.chat.id, sent_message.message_id)  # Удаление сообщения "Секундочку..."
-#     except Exception as e:
-#         pass
-
 def delete_last_message():
-    global sent_message
     try:
-        bot.delete_message(globalMessageObject.chat.id, sent_message.message_id)
+        bot.delete_message(globalMessageObject.chat.id, sent_message.message_id)  # Удаление сообщения "Секундочку..."
     except Exception as e:
         pass
-    finally:
-        sent_message = None  # обнуляем в любом случае
+
+# def delete_last_message():
+#     global sent_message
+#     try:
+#         bot.delete_message(globalMessageObject.chat.id, sent_message.message_id)
+#     except Exception as e:
+#         pass
+#     finally:
+#         sent_message = None  # обнуляем в любом случае
 
 def has_glyphs(text):
     for char in text:
