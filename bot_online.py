a81b61009ed40d4c72d5c80bab1fa0bf3ebac376
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2025-05-10 13:03:36 +0300

    Update bot_online.py

--- bot_online.py
+++ bot_online.py
@@ -177,10 +177,10 @@ def echo_all(message):
             
             if any(tag in response for tag in ['<pre>', '<b>']):
                 response = "abcde"
-                bot.reply_to(message, 'response', parse_mode='HTML')    # , parse_mode='HTML'
+                bot.reply_to(message, response, parse_mode='HTML')    # , parse_mode='HTML'
             else:
                 response = "abcde"
-                bot.reply_to(message, 'response')
+                bot.reply_to(message, response)
 
             break
 
