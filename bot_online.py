0ca0a9676cbc133cda648bce04c7a5c04bf6179d
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2025-05-12 12:25:20 +0300

    Update bot_online.py

--- bot_online.py
+++ bot_online.py
@@ -135,7 +135,8 @@ def echo_all(message):
 
     if aiContext.strip() == '':
         try:
-            temp_msg = bot.send_message(message.chat.id, "🧹  <i>История очищена</i>", parse_mode='HTML')
+            #temp_msg = bot.send_message(message.chat.id, "🧹  <i>История очищена</i>", parse_mode='HTML')
+            temp_msg = bot.send_message(message.chat.id, "🧹", parse_mode='HTML')
             time.sleep(2)
             bot.delete_message(message.chat.id, temp_msg.message_id)
         except Exception:
