79e3ec0905e5adab64516c632e56a6d613ab4dcc
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2025-05-12 11:04:50 +0300

    Update bot_online.py

--- bot_online.py
+++ bot_online.py
@@ -144,8 +144,7 @@ def echo_all(message):
             attempt_count += 1  # увеличение счетчика попыток
 
             if err != '':
-                print(  f'{datetime.now().strftime("[ %H:%M:%S ]")}:  {last_message}\n\nERROR:  {err}', flush=True  )
-                print(  f'•   •   •   •   •', flush=True  )
+                print(  f'\n\n{datetime.now().strftime("[ %H:%M:%S ]")}:  {last_message}\n\nERROR:  {err}\n\n•   •   •   •   •', flush=True  )
 
             if attempt_count > 1:
                 #sent_message = bot.reply_to(message, f'\n\n\n<i>⚙️  Секундочку... #{attempt_count} ({err})</i>', parse_mode='HTML')  # ответ 1
