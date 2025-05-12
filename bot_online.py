7c6e8393c64f5d167475454d549796a757f7cdad
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2025-05-12 16:33:58 +0300

    Update bot_online.py

--- bot_online.py
+++ bot_online.py
@@ -215,7 +215,7 @@ def echo_all(message):
             txt = messageText + " по-русски"
 
             usernameText = message.from_user.username or message.from_user.first_name or username
-            response = str( g4f_with_timeout(txt, usernameText, username) ).strip()
+            response = str( g4f_with_timeout(txt, username, usernameText) ).strip()
             if response == '':
                 time.sleep( 2 )
                 delete_last_message()
