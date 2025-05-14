7c0855d4bbd5577a60eda42a47e4567291ca21f8
Николай Т <94691024+nitkachuk@users.noreply.github.com>
2025-05-14 23:58:30 +0300

    Update state.py

--- state.py
+++ state.py
@@ -31,12 +31,18 @@ def has_glyphs( text ):
             return True
     return False
 
-def has_latins( text ):
+# def has_latins( text ):
+#     latins_count = len(re.findall(r'[a-zA-Z]', text))
+#     #total_count = len(text)
+    
+#     return latins_count > 10
+
+def has_latins(text):
     latins_count = len(re.findall(r'[a-zA-Z]', text))
-    #total_count = len(text)
-    
-    return latins_count > 10
-    
+    total_count = len(text)
+    if total_count == 0:
+        return False
+    return latins_count / total_count > 0.5    
 
 def escape_markdown_v2(text, plus_underline = 0):
     escape_chars = [  '[', ']', '(', ')', '~', 'Ⓝ', '>', '#', '+', '-', 
